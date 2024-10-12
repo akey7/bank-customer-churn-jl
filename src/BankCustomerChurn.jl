de2c9ec07184e6f45de91ccf98c987e2e8e47a1b
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-10-12 13:06:59 -0600

    Refactor frequency table.

--- src/BankCustomerChurn.jl
+++ src/BankCustomerChurn.jl
@@ -2,8 +2,10 @@ module BankCustomerChurn
 
 using CSV
 using DataFrames
+using FreqTables
+using CategoricalArrays
 
-export read_churn_data
+export read_churn_data, exited_vs_active_freqtable
 
 """
     read_churn_data(filename::String)
@@ -14,5 +16,21 @@ Read the churn data from the given filename and return the DataFrame
 - `filename::String`: Path to the CSV file
 """
 read_churn_data(filename::String) = CSV.read(filename, DataFrame)
+
+"""
+    exited_vs_active_freqtable(df::DataFrame)
+
+Create frequency table with "exited" on the rows and "is_active_member" on
+on the columns.
+
+# Arguments
+- `df::DataFrame`: DataFrame with churn data.
+"""
+function exited_vs_active_freqtable(df::DataFrame)
+    df_copy = copy(df)
+    df_copy.exited = categorical(df_copy.exited)
+    df_copy.is_active_member  = categorical(df_copy.is_active_member)
+    freqtable(df_copy, :exited, :is_active_member)
+end
 
 end
