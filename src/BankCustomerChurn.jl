16eeb99f25db319dc22ea224a6d05ae1ee3ba54b
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-10-13 13:50:13 -0600

    Coerce data inputs

--- src/BankCustomerChurn.jl
+++ src/BankCustomerChurn.jl
@@ -9,8 +9,6 @@ using MLJBase
 using Random
 using DecisionTree
 
-@load DecisionTreeClassifier pkg=DecisionTree
-
 export read_churn_data, exited_vs_active_freqtable
 
 """
@@ -21,7 +19,22 @@ Read the churn data from the given filename and return the DataFrame
 # Arguments
 - `filename::String`: Path to the CSV file
 """
-read_churn_data(filename::String) = CSV.read(filename, DataFrame)
+function read_churn_data(filename::String)
+    df = CSV.read(filename, DataFrame)
+    coerce(
+        df,
+        :credit_score => Continuous,
+        :estimated_salary => Continuous,
+        :age => Continuous,
+        :tenure => Continuous,
+        :balance => Continuous,
+        :female => Multiclass,
+        :num_of_products => Continuous,
+        :has_cr_card => Multiclass,
+        :is_active_member => Multiclass,
+        :exited => Multiclass
+    )
+end
 
 """
     exited_vs_active_freqtable(df::DataFrame)
@@ -37,39 +50,6 @@ function exited_vs_active_freqtable(df::DataFrame)
     df_copy.exited = categorical(df_copy.exited)
     df_copy.is_active_member = categorical(df_copy.is_active_member)
     freqtable(df_copy, :exited, :is_active_member)
-end
-
-function dt_classifier(df)
-    row_indices = 1:nrow(df)
-    train_indices, test_indices =
-        partition(row_indices, 0.8, shuffle = true, rng = MersenneTwister(123))
-    df_train = df[train_indices, :]
-    df_test = df[test_indices, :]
-    train_features, train_labels = unpack(df_train, ==(:exited))
-    dt_model = MLJDecisionTreeInterface.DecisionTreeClassifier()
-
-    max_depth_range = MLJBase.range(dt_model, :max_depth, lower = 2, upper = 20, scale = :linear, values = nothing)  # upper = 20
-    min_samples_split_range = MLJBase.range(dt_model, :min_samples_split, lower = 1, upper = 10, scale = :linear, values = nothing)  # upper = 10
-
-    tuning = TunedModel(
-        model = dt_model,
-        resampling = CV(nfolds = 5),
-        ranges = [
-            (:max_depth, max_depth_range),
-            (:min_samples_split, min_samples_split_range),
-        ],
-        measure = cross_entropy,
-        operation = :predict_mode,
-        tuning = Grid(resolution = 10),
-    )
-
-    tuned_machine = machine(tuning, train_features, train_labels)
-    # MLJ.fit!(tuned_machine)
-    # test_features, test_labels = unpack(df_test, ==(:exited))
-    # pred_labels = predict(tuned_machine, test_features)
-
-    # combined_df = DataFrame(df_test, true_label = test_labels, pred_label = pred_labels)
-
 end
 
 end
